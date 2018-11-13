c83a7ab794ed003202d8ad81826933e371f1f79d
Ilia Kopylov <iliakplv@gmail.com>
2018-11-13 19:06:31 +1300

    Refactor all the things

--- suprematism.py
+++ suprematism.py
@@ -6,34 +6,45 @@ plt.axis('equal')
 plt.axis('off')
 
 stroke_points = 2
-stroke_count = randint(3, 12)
-
-mean = 0
-std = 1.0
+normal_mean = 0
+normal_std = 1.0
+uniform_low = -1.0
+uniform_high = 1.0
 
 
 def rand_normal():
-    return np.random.normal(mean, std, size=stroke_points)
+    return np.random.normal(normal_mean, normal_std, size=stroke_points)
 
 
 def rand_uniform():
-    return np.random.uniform(low=-1.0, high=1.0, size=stroke_points)
+    return np.random.uniform(low=uniform_low, high=uniform_high, size=stroke_points)
 
 
-for i in range(stroke_count):
-    x_series = rand_normal()
-    y_series = rand_uniform()
+def get_width():
+    return randint(1, 20)
 
-    color_rnd = np.random.uniform()
-    if color_rnd > 0.9:
-        color = '#203080'
-    elif color_rnd > 0.8:
-        color = '#D02020'
-    elif color_rnd > 0.7:
-        color = '#D0D000'
+
+def get_color():
+    rnd = np.random.uniform(low=0.0, high=1.0)
+    if rnd > 0.9:
+        return '#203080'
+    elif rnd > 0.8:
+        return '#D02020'
+    elif rnd > 0.7:
+        return '#D0D000'
     else:
-        color = '#000000'
+        return '#000000'
 
-    plt.plot(x_series, y_series, linewidth=randint(1, 20), color=color)
 
-plt.show()
+def draw():
+    strokes = randint(3, 12)
+    for _ in range(strokes):
+        x_series = rand_normal()
+        y_series = rand_uniform()
+        plt.plot(x_series, y_series, linewidth=get_width(), color=get_color())
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    draw()
